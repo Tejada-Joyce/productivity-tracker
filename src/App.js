7b42650c5099cc71f0bd6bc95f9ecbce1bddb069
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-12 20:40:58 -0700

    Based nav structure set up with react routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from './components/Layout/Layout';
+import Home from "./components/Home/Home";
+import AddActivity from "./components/AddActivity/AddActivity";
+
 
 function App() {
   return (
-    <div>
-      Project App!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="addActivities" element={<AddActivity />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
---
+++ src/components/AddActivity/AddActivity.js
@@ -0,0 +1,7 @@
+const AddActivities = () => {
+    return (
+        <h1>Add Activities</h1>    
+    );
+}
+
+export default AddActivities;
