12e5b6719dcb0457a3b654f69bcd062d056485e9
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-13 23:06:42 -0700

    Params added and received in category dynamically

--- src/components/Layout/Navigation/Navigation.js
+++ src/components/Layout/Navigation/Navigation.js
@@ -1,36 +1,38 @@
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
-    //Create the list of links to be added to the drop down menu
-    const categories = ['Work', 'Exercise', 'Social Media', 'Entertainment', 'School'].map((categorie) => {
+    //Create the list of links to be added to the drop down menu with proper parms
+    const categories = ['Work', 'Exercise', 'Social Media', 'Entertainment', 'School'].map((categorie, key) => {
         return (
-            <Link className="dropdown-item" to={'/category?type=' + (categorie.charAt(0).toLowerCase()) + 
-                categorie.slice(1).replace(/\s/g, '')}>{categorie}</Link>
+            <Link className="dropdown-item" to={'/category/'+ (categorie.charAt(0).toLowerCase()) +
+            categorie.slice(1).replace(/\s/g, '')} key={key}>{categorie}</Link>
         )
+
+        // 
     });
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="nav-link" to="/">
         OurAppLogo
         </Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
         >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent" justify-content-end>
-        <ul class="navbar-nav ml-auto">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
             <li>
                 <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item dropdown">
-                <div class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li className="nav-item dropdown">
+                <div className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Categories
                 </div>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                     {categories}
                 </div>
             </li>
