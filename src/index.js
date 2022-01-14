5fb06993e28654e24cebcdb82b7b1cbab4774dd8
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-14 15:46:50 -0700

    Bug fix on drop fist click

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap/js/dist/dropdown';
+//import 'bootstrap/js/dist/dropdown';
 
 ReactDOM.render(
   <React.StrictMode>
