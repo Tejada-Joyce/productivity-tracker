a8ffc06a88073e16ef386181cfcda195bccfa39a
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-13 22:51:26 -0700

    added date time picker

--- src/components/AddActivity/AddActivity.js
+++ src/components/AddActivity/AddActivity.js
@@ -1,11 +1,17 @@
 import categories from "../../data/categories.json";
 import CategoriesList from "./CategoriesList";
+import TimeItem from "./TimeItem";
 const AddActivities = () => {
   return (
     <div>
-      <h1>Add Activities</h1>
-      <CategoriesList categories={categories} />
-      <input placeholder="Activity Name" />
+      <h1>Add Activity</h1>
+      <form>
+        <CategoriesList categories={categories} />
+        <input placeholder="Activity Name" />
+        <TimeItem label="Start Date/Time" />
+        <TimeItem label="End Date/Time" />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
--- src/components/AddActivity/TimeItem.js
+++ src/components/AddActivity/TimeItem.js
@@ -1,5 +1,24 @@
-const TimeItem = () => {
-  return <div></div>;
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import TextField from "@mui/material/TextField";
+import DateAdapter from "@mui/lab/AdapterMoment";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { useState } from "react";
+
+const TimeItem = ({ label }) => {
+  const [value, setValue] = useState(new Date());
+  return (
+    <LocalizationProvider dateAdapter={DateAdapter}>
+      <DateTimePicker
+        disableFuture
+        renderInput={(props) => <TextField {...props} />}
+        label={label}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </LocalizationProvider>
+  );
 };
 
 export default TimeItem;
