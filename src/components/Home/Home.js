01969ad77047b979523a4b9959aa60cbb2a4e6ad
Seth Bolander <bol15015@byui.edu>
2022-01-14 18:34:16 -0700

    added clicker for date change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,39 @@
 import { PieChart } from 'react-minimal-pie-chart';
-
-
+import { useState } from 'react';
 
 const Home = () => {
     const userData = require('../../productivity-data.json');
-    const transformUserData = (categories, activities) => {
+    const [startDate, setStartDate] = useState(new Date());
+
+    const changeDateHandler = (event) => {
+        if (event.target.value === 'true') {
+            setStartDate(prevState => {
+                let d = new Date(prevState);
+                d.setDate(d.getDate() + 1);
+                return d;
+            });
+        } else if (event.target.value === 'false') {
+            setStartDate(prevState => {
+                let d = new Date(prevState);
+                d.setDate(d.getDate() - 1);
+                return d;
+            });
+        }
+    };
+
+    const transformUserData = (categories, activities, startDate, endDate = null) => {
         return categories.map(cat => {
             // get total minutes for each activity in current category
-            let totalMinutes = activities.filter(act => act.category === cat.name)
-                .reduce((sum, act) => {
-                    console.log(act);
-                    
-                    let start = new Date(act.startTime);
-                    let end = new Date(act.endTime);
-                    let diffMs = (end - start); // milliseconds between startTime and endTime
-                    console.log(diffMs);
-                    let diffMins = Math.floor((diffMs/1000)/60); // transform to minutes
-                    console.log(diffMins);
-                    return sum + diffMins; // add to accumulator
-                }, 0);
+            let totalMinutes = activities.reduce((sum, act) => {
+                if (act.category !== cat.name) return sum; // filter out other categories
+                // filter out other days
 
-            console.log(`${cat.name} - ${totalMinutes}`);
+                let start = new Date(act.startTime);
+                let end = new Date(act.endTime);
+                let diffMs = (end - start); // milliseconds between startTime and endTime
+                let diffMins = Math.floor((diffMs/1000)/60); // transform to minutes
+                return sum + diffMins; // add to accumulator
+            }, 0);
     
             return {
                 key: cat.name,
@@ -29,17 +42,25 @@ const Home = () => {
             }
         });
     };
-    return <PieChart
-        animate
-        animationDuration={500}
-        animationEasing="ease-out"
-        center={[50, 50]}
-        data={transformUserData(userData.categories, userData.activities)}
-        radius={20}
-        label={({dataEntry}) => `${dataEntry.key} - ${Math.round(dataEntry.percentage)}%`}
-        labelPosition={70}
-        labelStyle={{fontSize: "1.5px"}}
-    />;
+
+    return (<>
+        <div>
+            <button onClick={changeDateHandler} value={false}>&#8592;</button>
+            <input type='text' value={startDate.toDateString()} disabled />
+            <button onClick={changeDateHandler} value={true}>&#8594;</button>
+        </div>
+        <PieChart
+            animate
+            animationDuration={500}
+            animationEasing="ease-out"
+            center={[50, 50]}
+            data={transformUserData(userData.categories, userData.activities, startDate)}
+            radius={20}
+            label={({dataEntry}) => `${dataEntry.key} - ${Math.round(dataEntry.percentage)}%`}
+            labelPosition={70}
+            labelStyle={{fontSize: "1.5px"}}
+        />
+    </>);
 }
 
 export default Home;
