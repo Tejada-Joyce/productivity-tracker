84fac8115ef092fe898b6bca5f25c691fc178aa5
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-15 14:36:33 -0700

    move category dropDown into a reusable component

--- src/components/Layout/Navigation/Navigation.js
+++ src/components/Layout/Navigation/Navigation.js
@@ -1,19 +1,11 @@
 import { Link } from "react-router-dom";
+import CatergoryDropDown from "../../Category/CatergoryDropDown"
 
 const Navigation = () => {
-    //Create the list of links to be added to the drop down menu with proper parms
-    const categories = ['Work', 'Exercise', 'Social Media', 'Entertainment', 'School'].map((categorie, key) => {
-        return (
-            <Link className="dropdown-item" to={'/category/'+ (categorie.charAt(0).toLowerCase()) +
-            categorie.slice(1).replace(/\s/g, '')} key={key}>{categorie}</Link>
-        )
-    });
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="nav-link" to="/">
-            OurAppLogo
-            </Link>
+            <Link className="nav-link" to="/">OurAppLogo</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
@@ -27,12 +19,7 @@ const Navigation = () => {
                     <Link className="nav-link" to="/">Home</Link>
                 </li>
                 <li className="nav-item dropdown">
-                    <div className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Categories
-                    </div>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        {categories}
-                    </div>
+                    <CatergoryDropDown />
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to="/addActivities">Add Activity</Link>
