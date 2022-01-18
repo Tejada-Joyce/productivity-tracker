083679911d6a19591bac430a24d88c7c984b84ec
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-18 16:06:50 -0700

    Content is grouped by category and date

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,9 +1,50 @@
 import { useParams } from "react-router-dom";
-import CategoryDropDown from "./CatergoryDropDown"
 import CategoryCard from "./CategoryCard";
+import DATA from "../../productivity-data.json";
 
-const Category = ({ match, location }) => {
+const Category = () => {
+    //Get the parameters passed on the routes
     const { category_type } = useParams();
+
+    /*TO DO: 
+        Create the fetch when data is available*/
+    //Get the productivity data 
+    const dataReceived = DATA.activities;
+ 
+    //Filter the content based on category_type
+    const categoryData = dataReceived.filter(entry => entry.category === category_type);
+
+    //Calculate the hour
+    const getHourDifference = (dateFuture, dateNow) => {
+        return (((dateFuture - dateNow) / 1000 / 3600) % 24)
+    }
+
+    //Hold the content based on date
+    const groupByDateCategory = new Map();
+
+    //Group the content based on the date with name and hours
+    categoryData.forEach(entry => {
+        let date = new Date(entry.startTime);
+
+        if (!groupByDateCategory.has(date.toDateString())) {
+            groupByDateCategory.set(date.toDateString(), [{
+                name: entry.name,
+                hours: getHourDifference(new Date(entry.endTime), date)
+            }])
+        } else {
+            groupByDateCategory.set(date.toDateString(), 
+            [
+                ...groupByDateCategory.get(date.toDateString()), 
+                {
+                    name: entry.name,
+                    hours: getHourDifference(new Date(entry.endTime), date)
+                }
+            ]);
+        }
+    });
+
+    //Pass the content to category dynamically to display multiple cards by date
+    console.log(groupByDateCategory)
 
     return (
         <>
