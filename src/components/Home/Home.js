173689e5c4dd5607fdd59fd9dc96093cdc7aed28
Seth Bolander <bol15015@byui.edu>
2022-01-18 20:19:42 -0700

    created DatePicker component and fixed startDate filtering

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,39 @@
 import { PieChart } from 'react-minimal-pie-chart';
 import { useState } from 'react';
+import DatePicker from './DatePicker';
+import moment from 'moment';
+
+import styles from './Home.module.css';
 
 const Home = () => {
     const userData = require('../../productivity-data.json');
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(moment());
 
     const changeDateHandler = (event) => {
         if (event.target.value === 'true') {
-            setStartDate(prevState => {
-                let d = new Date(prevState);
-                d.setDate(d.getDate() + 1);
-                return d;
-            });
+            setStartDate(prevState => moment(prevState).add(1, 'd'));
         } else if (event.target.value === 'false') {
-            setStartDate(prevState => {
-                let d = new Date(prevState);
-                d.setDate(d.getDate() - 1);
-                return d;
-            });
+            setStartDate(prevState => moment(prevState).subtract(1, 'd'));
         }
     };
 
     const transformUserData = (categories, activities, startDate, endDate = null) => {
         return categories.map(cat => {
-            // get total minutes for each activity in current category
+            // get total minutes for each activity in current category between startDate & endDate
             let totalMinutes = activities.reduce((sum, act) => {
                 if (act.category !== cat.name) return sum; // filter out other categories
-                // filter out other days
+                
+                let start = moment(act.startTime);
+                let end = moment(act.endTime);
 
-                let start = new Date(act.startTime);
-                let end = new Date(act.endTime);
-                let diffMs = (end - start); // milliseconds between startTime and endTime
-                let diffMins = Math.floor((diffMs/1000)/60); // transform to minutes
-                return sum + diffMins; // add to accumulator
+                // filter out other days
+                if (!startDate.isSame(start, 'day') && !startDate.isSame(end, 'day')) return sum;
+
+                // else add minutes to accumulator
+                let diffMins = start.diff(end, 'minutes');
+                return sum + diffMins;
             }, 0);
+
     
             return {
                 key: cat.name,
@@ -43,24 +43,20 @@ const Home = () => {
         });
     };
 
-    return (<>
-        <div>
-            <button onClick={changeDateHandler} value={false}>&#8592;</button>
-            <input type='text' value={startDate.toDateString()} disabled />
-            <button onClick={changeDateHandler} value={true}>&#8594;</button>
-        </div>
+    return (<div className={styles.PieContainer}>
+        <DatePicker clickHandler={changeDateHandler} date={startDate.format("dddd, MMM Do YYYY")} />
         <PieChart
+            className={styles.PieContainer__Pie}
             animate
             animationDuration={500}
             animationEasing="ease-out"
-            center={[50, 50]}
             data={transformUserData(userData.categories, userData.activities, startDate)}
-            radius={20}
-            label={({dataEntry}) => `${dataEntry.key} - ${Math.round(dataEntry.percentage)}%`}
+            radius={46}
+            label={({dataEntry}) => dataEntry.percentage === 0 ? "" : `${dataEntry.key} - ${Math.round(dataEntry.percentage)}%`}
             labelPosition={70}
-            labelStyle={{fontSize: "1.5px"}}
+            labelStyle={{fontSize: "3px"}}
         />
-    </>);
+    </div>);
 }
 
 export default Home;
