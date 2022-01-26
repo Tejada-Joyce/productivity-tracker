d92f38f8636d284ad37cc9dbfc4c041591977482
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-25 23:19:11 -0700

    modified style for navbar

--- src/components/Layout/Navigation/Navigation.js
+++ src/components/Layout/Navigation/Navigation.js
@@ -1,31 +1,45 @@
 import { Link } from "react-router-dom";
-import CatergoryDropDown from "../../Category/CatergoryDropDown"
+import CatergoryDropDown from "../../Category/CatergoryDropDown";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="nav-link" to="/"><img src="Logo.png" alt="Logo" style={{height:'50px'}}/></Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            >
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-                <li>
-                    <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item dropdown">
-                    <CatergoryDropDown />
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/addActivities">Add Activity</Link>
-                </li>
-            </ul>
-            </div>
-        </nav>
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-light ${styles.nav}`}>
+      <Link className="nav-link" to="/">
+        <img src="Logo.png" alt="Logo" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarSupportedContent"
+      >
+        <ul className="navbar-nav ml-auto">
+          <li>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item dropdown">
+            <CatergoryDropDown />
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/addActivities">
+              Add Activity
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
