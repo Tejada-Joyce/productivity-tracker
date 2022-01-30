6b6400da4f47da38ab82f73a97b441b5cf3f736a
Seth Bolander <bol15015@byui.edu>
2022-01-29 17:15:29 -0700

    fixed window reload (maybe)

--- src/components/Category/CategoryDetail.js
+++ src/components/Category/CategoryDetail.js
@@ -1,4 +1,5 @@
 import styles from "./CategoryDetail.module.css";
+import { useNavigate } from 'react-router-dom';
 import { config } from "../../firebaseConfig";
 
 const convertToJson = async (res) => {
@@ -10,28 +11,29 @@ const convertToJson = async (res) => {
   }
 };
 
-const deleteData = async (url) => {
-  try {
-    const options = {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(url, options).then(convertToJson);
-    return response;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const CategoryDetail = (props) => {
+  let navigate = useNavigate();
   const activitiesServer = `${config.db}activities`;
+
+  const deleteData = async (url) => {
+    try {
+      const options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch(url, options).then(convertToJson);
+      return response;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const deleteHandler = async (id) => {
     if (window.confirm("Are you sure you want to delete this activity?")) {
       await deleteData(`${activitiesServer}/${id}.json`);
-      window.location.reload();
+      navigate(0);
     }
   };
 
